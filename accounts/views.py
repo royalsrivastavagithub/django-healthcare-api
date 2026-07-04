31350e8dbd349068fece2d8bb79c62f64361a856
Royal Srivastava <royalsrivastavagithub@users.noreply.github.com>
2026-07-04 16:45:48 +0530

    Remove unused UserSerializer and clean up RegisterView

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,8 @@
 from rest_framework import generics, permissions
 
-from accounts.serializers import RegisterSerializer, UserSerializer
+from accounts.serializers import RegisterSerializer
 
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = (permissions.AllowAny,)
-
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        user = response.data
-        return response
