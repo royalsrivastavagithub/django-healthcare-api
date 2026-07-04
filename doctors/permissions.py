030aeac70c847b387faf9bc5c802a166689c97f7
Royal Srivastava <royalsrivastavagithub@users.noreply.github.com>
2026-07-04 16:28:30 +0530

    Add doctors app with serializers, views, permissions, and URL routing

---
+++ doctors/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsCreator(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.created_by == request.user
---
+++ doctors/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from doctors.models import Doctor
+
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = '__all__'
+        read_only_fields = ('created_by', 'created_at', 'updated_at')
